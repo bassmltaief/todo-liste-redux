2bdb2e44a9091c58c1ef80be67aa7896d0b4610f
bassmltaief <bassem.ltaief92@gmail.com>
2021-02-21 20:26:16 +0100

    first commit

---
+++ src/component/Addtask.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTask, } from '../js/action/';
+
+
+const Addtask = ({ handleFilter }) => {
+    const dispatch = useDispatch()
+    const [task, setTask] = useState('')
+    const AddNewTask = () => {
+        if (task) {
+            dispatch(addTask({ task: task, isDone: false, id: Math.random() }));
+            setTask('')
+        }
+        else alert('please enter a valid task')
+
+    }
+    return (
+        <div className='header'>
+            <h1>List of what we should do</h1>
+            <input type='text'
+                placeholder='entre a new task'
+                value={task}
+                onChange={(e) => setTask(e.target.value)} />
+            <div>
+                <button className="bat" onClick={
+                    AddNewTask
+                }
+                >Add</button>
+                <button className="bat" onClick={handleFilter}>All</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Addtask
---
+++ src/js/action/index.js
@@ -0,0 +1,33 @@
+import {ADD_TASK,DELETE_TASK,EDIT_TASK,COMPLETE_TASK} from '../const/ActionType';
+
+export const addTask=(payload)=>{
+    //payload : {task,isDone,id}
+    return{
+        type:ADD_TASK,
+        payload
+    }
+}
+
+export const deleteTask=(payload)=>{
+    //payload : {id}
+    return{
+        type:DELETE_TASK,
+        payload
+    }
+}
+
+export const completeTask=(payload)=>{
+    //payload : {id}
+    return{
+        type:COMPLETE_TASK,
+        payload
+    }
+}
+
+export const editTask=(payload)=>{
+    //payload : {taskModified,id}
+    return{
+        type:EDIT_TASK,
+        payload
+    }
+}
